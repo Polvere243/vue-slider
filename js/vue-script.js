c115de80cfeac2df6489b46b13ebfa05a35dde0e
Polvere243 <danielercoli@gmail.com>
2024-01-22 16:36:18 +0100

    imported vue js and checked connection

---
+++ js/vue-script.js
@@ -0,0 +1,15 @@
+console.log("Vue Ok", Vue);
+
+const {createApp} = Vue;
+const app = createApp({
+    name: "vue slider",
+    data() {
+        return {
+            
+        }
+             
+        
+    }
+})
+
+app.mount ("#root");
